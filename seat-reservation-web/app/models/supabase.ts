216d470345a325bb8a5ce13a376486ff4db65c3f
bnitech <dongbin88@gmail.com>
2023-06-26 01:39:29 +0900

    [dongbin] create new project

--- seat-reservation-web/app/models/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import * as process from 'process';
-import { Database } from './database.types';
-
-export default createClient<Database>(
-  process.env.SUPARBASE_URL as string,
-  process.env.SUPARBASE_ANON_KEY as string,
-);
