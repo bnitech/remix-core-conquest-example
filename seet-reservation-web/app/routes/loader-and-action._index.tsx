f55622be923ca8f8b244c07b3bf0abe99404b41f
bnitech <dongbin88@gmail.com>
2023-06-26 01:15:06 +0900

    [dongbin] action #2
    1

--- seet-reservation-web/app/routes/loader-and-action._index.tsx
+++ seet-reservation-web/app/routes/loader-and-action._index.tsx
@@ -1,33 +1,50 @@
-import { json, LoaderFunction } from "@remix-run/node";
-import {useLoaderData} from "@remix-run/react";
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
 
 type LoaderData = {
-    status: number;
-    message: string;
-}
-
-export const loader: LoaderFunction = async ({ request, params }) => {
-    console.log("해당 console.log 는 터미널 (Remix Server) 에서만 나옵니다.")
-
-    // Request 에 있는 Cookie 가져오는 법
-    const cookie = request.headers.get("Cookie");
-
-    // URL Query `?query=` 로 들어온 값을 가져오는 방법
-    const url = new URL(request.url);
-    const query = url.searchParams.get("test");
-
-    console.log("Cookie", cookie)
-    console.log("URL", url);
-    console.log("Query", query);
-
-    // => 위와 동일한 코드, json 은 Response 객체를 쉽게 만들어 주는 Helper 함수입니다.
-    return json<LoaderData>({
-        status: 200,
-        message: "Hello World",
-    });
+  status: number;
+  message: string;
 };
 
-export default function LoaderAndAction_index(){
-    const initalData = useLoaderData<LoaderData>()
-    return <div>{JSON.stringify(initalData)}</div>
+export const action: ActionFunction = async ({ request, params }) => {
+  console.log('Action 실행됨');
+  const body = await request.formData();
+  const name = body.get('name');
+  console.log(name);
+  return redirect(`/loader-and-action`);
+};
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  console.log('해당 console.log 는 터미널 (Remix Server) 에서만 나옵니다.');
+
+  const cookie = request.headers.get('Cookie');
+  const url = new URL(request.url);
+  const query = url.searchParams.get('test');
+
+  // console.log('Cookie', cookie);
+  // console.log('URL', url);
+  // console.log('Query', query);
+
+  return json<LoaderData>({
+    status: 200,
+    message: 'Hello World',
+  });
+};
+
+export default function LoaderAndAction_index() {
+  const initalData = useLoaderData<LoaderData>();
+  return (
+    <div>
+      {JSON.stringify(initalData)}
+      <Form method="post">
+        <input type="text" name="name" />
+        <button type="button">전송</button>
+      </Form>
+    </div>
+  );
 }
