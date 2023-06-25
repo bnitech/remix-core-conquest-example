104fcf0dfbc8e25485f45db7165d1ea4210009b1
bnitech <dongbin88@gmail.com>
2023-06-26 01:08:21 +0900

    [dongbin] feat: apply tslint & prettier

--- seet-reservation-web/.eslintrc.js
+++ seet-reservation-web/.eslintrc.js
@@ -1,4 +1,14 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    'airbnb',
+    `prettier`,
+  ],
+  rules: {
+    // 'react/prefer-stateless-function': 0,
+    // 'react/jsx-filename-extension': 0,
+    // 'react/jsx-one-expression-per-line': 0,
+  },
 };
